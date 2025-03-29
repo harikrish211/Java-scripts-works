cfa26dd602e0be370232301971e33c0e70be59ae
harikrish211 <harikrishnapradeep2121@gmail.com>
2025-03-29 14:34:44 +0530

    incuded reset function plus warnings

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 
-
-function convert()
+var count=0;
+function convert(event)
 {
+
+    event.preventDefault();
+    count++;
     const inch=document.getElementById('inch').value;
+    if(count<=1)
+    {
+    
     let cm=inch*2.54;
     document.getElementById('inch').value=cm;
+
+    }
+    else
+    {
+        alert("You are trying to convert a converted value");
+        alert("Please reset and try!!")
+
+        
+    }
     
 
+}
+
+function rest()
+{
+    count=0;
+    document.getElementById('inch').value=null;
 
 }
 
